e03a01ba60727b94e8f24ebeaf96018939a43008
alireza-2030 <alireza-2030@users.noreply.github.com>
2026-06-01 05:39:00 +0330

    Fix System.ArgumentNullException in Program.cs by adding null safety checks

--- 3xui-multi-protocol/Program.cs
+++ 3xui-multi-protocol/Program.cs
@@ -1,173 +1,253 @@
 using Newtonsoft.Json;
 
-
-
-
 while (true)
 {
-
-    using var db = new MultiProtocolContext();
-    var Clients = db.Client_Traffics
-       .ToList();
-    if (!File.Exists("LocalDB.json"))
+    try
     {
-        localDB local = new localDB() { Sec = 10, clients = Clients };
-        var LocalD =File.Create("LocalDB.json");
-        using( var writer = new StreamWriter(LocalD))
+        using var db = new MultiProtocolContext();
+        var Clients = db.Client_Traffics.ToList();
+        if (Clients == null) Clients = new List<Client_Traffics>();
+
+        if (!File.Exists("LocalDB.json"))
         {
-            writer.Write(JsonConvert.SerializeObject(local));
-        }
-        LocalD.Close();
-    }
-    localDB localDB = JsonConvert.DeserializeObject<localDB>(File.ReadAllText("LocalDB.json"));
-
-   
-    List<Client> ALLClients = new List<Client>();
-
-    var inbounds = db.Inbounds.ToList();
-    foreach (var item in inbounds)
-    {
-        inboundsetting setting = JsonConvert.DeserializeObject<inboundsetting>(item.Settings);
-        ALLClients.AddRange(setting.clients);
-    }
-
-    List<Client> FinalClients = new List<Client>();
-    List<Client_Traffics> FinalClients_Traffic = new List<Client_Traffics>();
-
-    foreach (var client in ALLClients)
-    {
-        if (!FinalClients.Any(x => x.subId == client.subId))
-        {
-            if (ALLClients.Where(x => x.subId == client.subId).Count() > 1)
+            localDB local = new localDB() { Sec = 10, clients = Clients };
+            var LocalD = File.Create("LocalDB.json");
+            using (var writer = new StreamWriter(LocalD))
             {
+                writer.Write(JsonConvert.SerializeObject(local));
+            }
+            LocalD.Close();
+        }
 
-                List<Client> Calculate = ALLClients.Where(x => x.subId == client.subId).ToList();
-                List<Client_Traffics> Calculate2 = new List<Client_Traffics>();
-                foreach (var client2 in Calculate)
-                {
-                    Calculate2.Add(Clients.Where(x => x.Email == client2.email).FirstOrDefault());
-                }
+        localDB localDB = null;
+        try
+        {
+            localDB = JsonConvert.DeserializeObject<localDB>(File.ReadAllText("LocalDB.json"));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error reading LocalDB.json: " + ex.Message);
+        }
+        if (localDB == null)
+        {
+            localDB = new localDB() { Sec = 10, clients = new List<Client_Traffics>() };
+        }
+        if (localDB.clients == null)
+        {
+            localDB.clients = new List<Client_Traffics>();
+        }
 
-                Int64? maxTotalGB = Calculate.Max(x => x.totalGB);
-                Int64? maxTotal = Calculate2.Max(x => x.Total);
+        List<Client> ALLClients = new List<Client>();
 
-                Int64? maxUP = Calculate2.Max(x => x.Up);
-                Int64? maxDOWN = Calculate2.Max(x => x.Down);
-                Int64? UP = 0;
-                Int64? DOWN = 0;
-
-                Int64? DateMax= Calculate2.Max(x => x.Expiry_Time);
-                Int64? DateMin= Calculate2.Min(x => x.Expiry_Time);
-                Int64? ExpireTime=0;
-                if (DateMax > 0)
-                {
-                    ExpireTime = DateMax;
-                }
-                else if (DateMin < 0)
-                    ExpireTime = DateMin;
+        var inbounds = db.Inbounds.ToList();
+        if (inbounds != null)
+        {
+            foreach (var item in inbounds)
+            {
+                if (item == null || string.IsNullOrEmpty(item.Settings)) continue;
                 try
                 {
-                    foreach (var client2 in Calculate2)
+                    inboundsetting setting = JsonConvert.DeserializeObject<inboundsetting>(item.Settings);
+                    if (setting != null && setting.clients != null)
                     {
-                        
-                        if (client2.Up != maxUP)
-                        {
-                            Int64? oldusage = localDB.clients.Where(x => x.Email == client2.Email).First().Up;
-                            if (client2.Up > oldusage && oldusage != 0 && oldusage != null)
-                                UP += client2.Up - oldusage;
-                        }
-                        if (client2.Down != maxDOWN)
-                        {
-                            Int64? oldusage = localDB.clients.Where(x => x.Email == client2.Email).First().Down;
+                        ALLClients.AddRange(setting.clients.Where(x => x != null));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Parse inbound settings error: " + e.Message);
+                }
+            }
+        }
 
-                            if (client2.Down > oldusage && oldusage != 0 && oldusage != null)
-                                DOWN += client2.Down - oldusage;
+        List<Client> FinalClients = new List<Client>();
+        List<Client_Traffics> FinalClients_Traffic = new List<Client_Traffics>();
+
+        foreach (var client in ALLClients)
+        {
+            if (client == null || string.IsNullOrEmpty(client.subId)) continue;
+            if (!FinalClients.Any(x => x != null && x.subId == client.subId))
+            {
+                if (ALLClients.Where(x => x != null && x.subId == client.subId).Count() > 1)
+                {
+                    List<Client> Calculate = ALLClients.Where(x => x != null && x.subId == client.subId).ToList();
+                    List<Client_Traffics> Calculate2 = new List<Client_Traffics>();
+                    foreach (var client2 in Calculate)
+                    {
+                        if (client2 == null) continue;
+                        var traffic = Clients.Where(x => x != null && x.Email == client2.email).FirstOrDefault();
+                        if (traffic != null)
+                        {
+                            Calculate2.Add(traffic);
+                        }
+                    }
+
+                    if (Calculate2.Count > 0)
+                    {
+                        Int64? maxTotalGB = Calculate.Max(x => x.totalGB);
+                        Int64? maxTotal = Calculate2.Max(x => x.Total);
+
+                        Int64? maxUP = Calculate2.Max(x => x.Up);
+                        Int64? maxDOWN = Calculate2.Max(x => x.Down);
+                        Int64? UP = 0;
+                        Int64? DOWN = 0;
+
+                        Int64? DateMax = Calculate2.Max(x => x.Expiry_Time);
+                        Int64? DateMin = Calculate2.Min(x => x.Expiry_Time);
+                        Int64? ExpireTime = 0;
+                        if (DateMax > 0)
+                        {
+                            ExpireTime = DateMax;
+                        }
+                        else if (DateMin < 0)
+                        {
+                            ExpireTime = DateMin;
+                        }
+
+                        try
+                        {
+                            foreach (var client2 in Calculate2)
+                            {
+                                if (client2 == null) continue;
+                                if (client2.Up != maxUP)
+                                {
+                                    var localClient = localDB.clients.Where(x => x != null && x.Email == client2.Email).FirstOrDefault();
+                                    if (localClient != null)
+                                    {
+                                        Int64? oldusage = localClient.Up;
+                                        if (client2.Up > oldusage && oldusage != 0 && oldusage != null)
+                                        {
+                                            UP += client2.Up - oldusage;
+                                        }
+                                    }
+                                }
+                                if (client2.Down != maxDOWN)
+                                {
+                                    var localClient = localDB.clients.Where(x => x != null && x.Email == client2.Email).FirstOrDefault();
+                                    if (localClient != null)
+                                    {
+                                        Int64? oldusage = localClient.Down;
+                                        if (client2.Down > oldusage && oldusage != 0 && oldusage != null)
+                                        {
+                                            DOWN += client2.Down - oldusage;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Traffic calculation error: " + e.Message);
+                        }
+
+                        foreach (var cal2 in Calculate2)
+                        {
+                            if (cal2 == null) continue;
+                            cal2.Total = maxTotal;
+                            cal2.Up = maxUP + UP;
+                            cal2.Down = maxDOWN + DOWN;
+                            cal2.Expiry_Time = ExpireTime;
+                            FinalClients_Traffic.Add(cal2);
+                        }
+                        foreach (var cal in Calculate)
+                        {
+                            if (cal == null) continue;
+                            cal.totalGB = maxTotalGB;
+                            cal.expiryTime = ExpireTime;
+                            FinalClients.Add(cal);
                         }
                     }
                 }
-                catch (Exception e) { Console.WriteLine(e.Message); }
-
-                bool check = false;
-                if (Calculate2.Where(x => x.Enable == true).Count() == 1)
-                {
-                    check = true;
-                }
-
-                foreach (var cal2 in Calculate2)
-                {
-                    cal2.Total = maxTotal;
-                    cal2.Up = maxUP+UP;
-                    cal2.Down = maxDOWN+DOWN;
-                    cal2.Expiry_Time = ExpireTime;
-                    FinalClients_Traffic.Add(cal2);
-
-                }
-                foreach (var cal in Calculate)
-                {
-                    cal.totalGB = maxTotalGB;
-                    cal.expiryTime = ExpireTime;
-                    FinalClients.Add(cal);
-                }
             }
-
         }
 
-    }
-
-
-    db.Client_Traffics.UpdateRange(FinalClients_Traffic);
-
-    List<Inbound> FinalInbounds = new List<Inbound>();
-    try {
-        foreach (var inbound in db.Inbounds)
+        if (FinalClients_Traffic.Count > 0)
         {
-            if(inbound.Protocol== "vmess" || inbound.Protocol == "vless")
-            {
-                inboundsetting setting = JsonConvert.DeserializeObject<inboundsetting>(inbound.Settings);
-                var clis = FinalClients_Traffic.Where(x => x.Inbound_Id == inbound.Id).ToList();
-                List<Client> addtoInbound = new List<Client>();
-                foreach (var client in clis)
-                {
-                    addtoInbound.Add(FinalClients.Where(x => x.email == client.Email).FirstOrDefault());
-
-                }
-                if (addtoInbound.Count() > 0)
-                {
-
-                    List<Client> pastclients = new List<Client>();
-                    foreach (Client client in setting.clients)
-                        if (!addtoInbound.Any(x => x.email == client.email)) { pastclients.Add(client); }
-                    pastclients.AddRange(addtoInbound);
-                    setting.clients = pastclients;
-                    inbound.Settings = JsonConvert.SerializeObject(setting);
-                    FinalInbounds.Add(inbound);
-                }
-            }
-            
+            db.Client_Traffics.UpdateRange(FinalClients_Traffic);
         }
 
+        List<Inbound> FinalInbounds = new List<Inbound>();
+        try
+        {
+            foreach (var inbound in db.Inbounds)
+            {
+                if (inbound == null || string.IsNullOrEmpty(inbound.Settings)) continue;
+                if (inbound.Protocol == "vmess" || inbound.Protocol == "vless" || inbound.Protocol == "trojan")
+                {
+                    inboundsetting setting = JsonConvert.DeserializeObject<inboundsetting>(inbound.Settings);
+                    if (setting == null) continue;
+                    var clis = FinalClients_Traffic.Where(x => x != null && x.Inbound_Id == inbound.Id).ToList();
+                    List<Client> addtoInbound = new List<Client>();
+                    foreach (var client in clis)
+                    {
+                        if (client == null) continue;
+                        var matchedCli = FinalClients.Where(x => x != null && x.email == client.Email).FirstOrDefault();
+                        if (matchedCli != null)
+                        {
+                            addtoInbound.Add(matchedCli);
+                        }
+                    }
+                    if (addtoInbound.Count > 0)
+                    {
+                        List<Client> pastclients = new List<Client>();
+                        if (setting.clients != null)
+                        {
+                            foreach (Client client in setting.clients)
+                            {
+                                if (client == null) continue;
+                                if (!addtoInbound.Any(x => x != null && x.email == client.email))
+                                {
+                                    pastclients.Add(client);
+                                }
+                            }
+                        }
+                        pastclients.AddRange(addtoInbound);
+                        setting.clients = pastclients;
+                        inbound.Settings = JsonConvert.SerializeObject(setting);
+                        FinalInbounds.Add(inbound);
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Update inbounds error: " + e.Message);
+        }
+
+        if (FinalInbounds.Count > 0)
+        {
+            db.Inbounds.UpdateRange(FinalInbounds);
+        }
+        db.SaveChanges();
+
+        var client_Traffics = new MultiProtocolContext().Client_Traffics.ToList();
+        if (client_Traffics == null) client_Traffics = new List<Client_Traffics>();
+
+        localDB updateLocal = new localDB() { Sec = localDB.Sec, clients = client_Traffics };
+        try
+        {
+            if (File.Exists("LocalDB.json"))
+            {
+                File.Delete("LocalDB.json");
+            }
+            var file = File.Create("LocalDB.json");
+            using (StreamWriter streamWriter = new StreamWriter(file))
+            {
+                streamWriter.Write(JsonConvert.SerializeObject(updateLocal));
+            }
+            file.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error writing LocalDB.json: " + e.Message);
+        }
+
+        Console.WriteLine("Synchronization done successfully.");
     }
-    catch (Exception e) { Console.WriteLine(e.Message); }
-    db.Inbounds.UpdateRange(FinalInbounds);
-    db.SaveChanges();
-    var client_Traffics = new MultiProtocolContext().Client_Traffics
-       .ToList();
+    catch (Exception ex)
+    {
+        Console.WriteLine("Global synchronization iteration error: " + ex.Message);
+    }
 
-     localDB updateLocal = new localDB() { Sec = localDB.Sec, clients = client_Traffics };
-    File.Delete("LocalDB.json");
-    var file = File.Create("LocalDB.json");
-    StreamWriter streamWriter = new StreamWriter(file);
-        streamWriter.Write(JsonConvert.SerializeObject(updateLocal));
-        streamWriter.Close();
-    file.Close();
-
-    Console.WriteLine("Done");
     Thread.Sleep(25 * 1000);
-
 }
-
-
-
-
-
-
